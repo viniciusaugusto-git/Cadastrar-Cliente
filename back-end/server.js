bf7e8241b7e162c3f951dd2a7e8c05d35f0af360
Fabricio Langer <fabriciolanger@live.com>
2026-04-02 10:59:42 -0400

    upd-back-end: ajuste para utilizar a rota de clientes

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,22 @@
 import express from "express";
+import cors from "cors";
 import path from "path";
+import routerClient from "./routes/clients.route.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cors());
 
-// 👉 SERVIR ARQUIVOS DO FRONT
 app.use(express.static(path.join(__dirname, "../front-end")));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../front-end/index.html"));
 });
+
+// ROTAS CLIENTE
+app.use("/api/client", routerClient);
 
 app.listen(3000, () => {
     console.log("Rodando em http://localhost:3000");
